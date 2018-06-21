371e598a989aa1ccaf9511b287c1c6816147f12f
BIT Sutdent <classroom@bgit.rs>
2018-06-21 12:48:46 +0200

    header done,service done

--- src/services/videoservice.js
+++ src/services/videoservice.js
@@ -3,13 +3,7 @@ import { url } from '../shared/constants';
 class VideoService {
 
     getSearchVideo(text) {
-        return fetch(`${url}${text}`, {
-            'PageInfo': {
-                'ResultPerPage': 20
-            }
-        }).then(response => {
-
-
+        return fetch(`${url}${text}`).then(response => {
             return response.json()
         }).then(data => {
             return data
