dec6caf2f059867a324367b28cfc0173f848459f
Mirjana Radulović <mirjanaradulovic@src.si>
2018-06-22 00:05:36 +0200

    prev video done

---
+++ src/app/Feed/NoPreviousVideos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NoPreviousVideos.css';
+import img from '../../images/img.png'
+
+export const NoPreviousVideos=()=>{
+    return (
+        <div>
+            <div className="container">
+            <div className="sad_icon"><img src={img} id="sadface" /></div>
+            <p className="sad_icon"> No videos watched</p>
+        </div>
+        </div>
+    )
+}
---
+++ src/app/partials/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Loading.css'
+
+export const Loading = () => {
+    return (
+        <div class="spinner">
+            <div class="rect1"></div>
+            <div class="rect2"></div>
+            <div class="rect3"></div>
+            <div class="rect4"></div>
+            <div class="rect5"></div>
+        </div>
+    )
+}
