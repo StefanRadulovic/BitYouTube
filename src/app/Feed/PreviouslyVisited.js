f7d673d6e40428d5e1a5da3f5b290aba8a6116da
BIT Sutdent <classroom@bgit.rs>
2018-06-22 15:07:15 +0200

    app build

--- src/app/Feed/PreviouslyVisited.js
+++ src/app/Feed/PreviouslyVisited.js
@@ -1,21 +1,21 @@
-import React,{Fragment} from 'react';
+import React, { Fragment } from 'react';
 import { SuggestedVideos } from './SuggestedVideos';
-import {NoPreviousVideos} from './NoPreviousVideos';
+import { NoPreviousVideos } from './NoPreviousVideos';
 import './PreviouslyVisited.css'
 
 
-const PreviouslyVisitedVideos =(props)=> {
-    
-        let local=localStorage.getItem('videos')
-        const videos=JSON.parse(local)
-        return (
-            
-            <Fragment>
-                <p id='previous'>Previous</p>
-            {local && videos.length!==1 ? <SuggestedVideos videos={videos.slice(1,videos.length)} onClickHandler={props.onClickHandler} /> :<NoPreviousVideos/> }
-            </Fragment>
-        )
-    }
- 
+const PreviouslyVisitedVideos = (props) => {
+
+    let local = localStorage.getItem('videos')
+    const videos = JSON.parse(local)
+    return (
+
+        <Fragment>
+            <p id='previous'>Previous</p>
+            {local && videos.length !== 1 ? <SuggestedVideos videos={videos.slice(1, videos.length)} onClickHandler={props.onClickHandler} /> : <NoPreviousVideos />}
+        </Fragment>
+    )
+}
+
 
 export default PreviouslyVisitedVideos;
